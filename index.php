26fc5c46abdc80daae8006b5804ef4b6e84b309b
demorite <dylan3396@hotmail.fr>
2016-05-24 11:41:09 +0200

    Implémentation de la page Login

--- index.php
+++ index.php
@@ -1,41 +1,41 @@
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-  <link rel="icon" href="favicon.ico"/>
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen"/>
-  <link type="text/css" rel="stylesheet" href="css/style.css"/>
-  <link type="text/css" rel="stylesheet" href="css/animate.css"/>
-  <link type="text/css" rel="stylesheet" href="css/hover.css" media="all">
-  <script src="js/jquery-1.12.3.min.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Lekkers</title>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <link rel="icon" href="favicon.ico"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <link type="text/css" rel="stylesheet" href="css/animate.css"/>
+    <link type="text/css" rel="stylesheet" href="css/hover.css" media="all">
+    <script src="js/jquery-1.12.3.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Lekkers</title>
 </head>
 
 
 <body>
-  <nav>
+<nav>
     <div class="nav-wrapper">
-      <a href="http://lekkersfrance.com" class="brand-logo">Lekkers</a>
-      <ul id="nav-mobile" class="hide-on-med-and-down">
-        <li><a href="qualite.html">La qualité</a></li>
-        <li><a href="carte.html">La carte</a></li>
-        <li><a href="info.html">Info/Event</a></li>
-        <li><a href="info.html">Contact</a></li>
-      </ul>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul class="side-nav" id="mobile-demo">
-        <li><a href="qualite.html">La qualité</a></li>
-        <li><a href="carte.html">La carte</a></li>
-        <li><a href="info.html">Info/Event</a></li>
-        <li><a href="info.html">Contact</a></li>
-      </ul>
+        <a href="http://lekkersfrance.com" class="brand-logo">Lekkers</a>
+        <ul id="nav-mobile" class="hide-on-med-and-down">
+            <li><a href="qualite.html">La qualité</a></li>
+            <li><a href="carte.html">La carte</a></li>
+            <li><a href="info.html">Info/Event</a></li>
+            <li><a href="info.html">Contact</a></li>
+        </ul>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="side-nav" id="mobile-demo">
+            <li><a href="qualite.html">La qualité</a></li>
+            <li><a href="carte.html">La carte</a></li>
+            <li><a href="info.html">Info/Event</a></li>
+            <li><a href="info.html">Contact</a></li>
+        </ul>
     </div>
 
     <div class="acceuil">
-      <img src="img/acceuil.png" alt="" />
+        <img src="img/acceuil.png" alt=""/>
     </div>
 
 
@@ -43,12 +43,12 @@
     <script type="text/javascript" src="js/materialize.min.js"></script>
 
     <script>
-    $(document).ready(function () {
-      $(".button-collapse").sideNav();
-    });
-    $(document).ready(function () {
-      $('select').material_select();
-    });
+        $(document).ready(function () {
+            $(".button-collapse").sideNav();
+        });
+        $(document).ready(function () {
+            $('select').material_select();
+        });
     </script>
-  </body>
-  </html>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <link rel="icon" href="favicon.ico"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <link type="text/css" rel="stylesheet" href="css/styleLogin.css"/>
+    <link type="text/css" rel="stylesheet" href="css/animate.css"/>
+    <link type="text/css" rel="stylesheet" href="css/hover.css" media="all">
+    <script src="js/jquery-1.12.3.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>lekkersLogin</title>
+</head>
+<body>
+
+<?php if (empty($_POST['login'])) { ?>
+    <div class="nav-wrapper" style="margin-bottom: 10px">
+        <div class="white-text" style="font-size: 2em; margin-left: 10px;">Login</div>
+    </div>
+    <form class="col s12" action="login.php" method="post">
+        <div class="row">
+            <div class="input-field col s12 ">
+                <input placeholder="Login" id="login" type="text" name="login" class="validate">
+                <label for="login">Login</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input placeholder="Password" id="password" type="password" name="password" class="validate">
+                <label for="password">Password</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class=" col s6">
+                <button class="btn red darken-1 waves-effect waves-light" type="submit" name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </div>
+    </form>
+    <!--    Code en attendant la BDD -->
+<?php } elseif ($_POST['login'] == 'admin' && sha1($_POST['password']) == "ebfc7910077770c8340f63cd2dca2ac1f120444f") { ?>
+    <div class="nav-wrapper">
+        <div class="white-text" style="font-size: 2em; margin-left: 10px;">Bienvenue Admin</div>
+    </div>
+    <form action="login.php" method="post">
+        <div class="col s6" style="margin-top: 5px">
+            <button class="btn red darken-2 waves-effect waves-light" type="submit" name="action">Retour à la page login
+                <i class="material-icons right">send</i>
+            </button>
+        </div>
+    </form>
+    <ul class="collapsible" data-collapsible="accordion">
+        <li>
+            <div class="collapsible-header"><i class="material-icons">view_module</i>Gestion des images</div>
+            <div class="collapsible-body">
+                <p>Mettre la gestion des images ici (Dylan)</p>
+            </div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">playlist_add</i>Section à ajouter</div>
+            <div class="collapsible-body">
+                <p>Rien pour le moment</p>
+            </div>
+        </li>
+    </ul>
+<?php }
+else ?>
+</body>
+</html>
